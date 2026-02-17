93f04027c3382e6a866f807b1ca512bc5f2696c6
sableHoujuu <dktpoole3@gmail.com>
2026-02-17 11:57:07 -0500

    implemented the main guts

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import argparse
+import os
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("No API key found. Did you remember to set it?")
+
+    parser = argparse.ArgumentParser(description="Agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+    if response.usage_metadata is None:
+        raise RuntimeError("No response recieved, is the API down?")
+    if args.verbose is True:
+        output = (
+            f"User prompt: {args.user_prompt}\n"
+            f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+            f"Response tokens: {response.usage_metadata.candidates_token_count}\n"
+        )
+        print(output)
+    print(f"Response: {response.text}")
 
 
 if __name__ == "__main__":
